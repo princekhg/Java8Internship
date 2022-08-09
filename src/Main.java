72fc2808728cde8687fc6702795833b71a353542
Shubham Prince <44049342+princekhg@users.noreply.github.com>
2022-08-09 14:47:01 +0530

    these are the new changes to the main class codes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
+// this is the question
+
+public class Main extends Thread {
+    int[] arr = {1, 2, 3, 4, 5};
+
+    public void run() {
+        for (int i : arr) {
+            System.out.println("From Thread A -> " + i);
+        }
+    }
+
+}
+
+// Second Thread Class
+class MultiThreadSec extends Main{
+    public void run(){
+        for(int i:arr)
+            System.out.println("From Thread B ->" +i);
+    }
+}
+// Runner Thread Class Executing the other thread classes
+ class RunnerClassThreadA {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().start();
+        new MultiThreadSec().start();
     }
 }
